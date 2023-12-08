707155ba5fb96a6888c6eada36fd688d023bffc6
DONENAZIBA JOSIAH <jdonenaziba@gmail.com>
2023-12-08 21:46:22 +0100

    completed adding entity framework

---
+++ StudentEnrollment.Data/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentEnrollment.Data
+{
+    public class Course : BaseEntity
+    {
+        public string Title { get; set; }   
+        public int Credits { get; set; }
+    }
+
+
+}
---
+++ StudentEnrollment.Data/Migrations/20231208204118_SeededDefaultRolesAndCourses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StudentEnrollment.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededDefaultRolesAndCourses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "7a66ada6-e903-4883-bb49-6db1076f50eb", null, "user", "USER" },
+                    { "bda4e022-cc27-4d97-8c02-eb32c52f7dab", null, "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "CreatedBy", "CreatedDate", "Credits", "ModifiedBy", "ModifiedDate", "Title" },
+                values: new object[,]
+                {
+                    { 1, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Minimal API Development" },
+                    { 2, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 4, "", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Minimal API Development" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7a66ada6-e903-4883-bb49-6db1076f50eb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "bda4e022-cc27-4d97-8c02-eb32c52f7dab");
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
